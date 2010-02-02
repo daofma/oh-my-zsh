6c46a739ade463e971989d2f2f314d3af05623f7
David Arnold <daofma@gmail.com>
2010-02-02 13:54:07 -0500

    Made scm_info configurable by theme updated theme to take configuration into account

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,22 +1,46 @@
 # get the name of the branch we are on
 function git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_SCM_PROMPT_PREFIX$ZSH_THEME_SCM_BRANCH_PREFIX${ref#refs/heads/}$ZSH_THEME_SCM_BRANCH_SUFFIX$(parse_git_dirty)$ZSH_THEME_SCM_PROMPT_SUFFIX"
+	gitstat=$(git status 2>/dev/null)
+
+	if [[ $? -eq 128 ]]; then
+		return
+	fi
+
+	zsh_scm_info_branch="$ZSH_THEME_SCM_BRANCH_PREFIX$(current_branch)$ZSH_THEME_SCM_BRANCH_SUFFIX"
+	zsh_scm_info_tag=""
+	zsh_scm_info_dirty=$(parse_git_dirty $gitstat)
+	zsh_scm_info_branch_status=$(parse_git_branch_status $gitstat)
+	zsh_scm_info_patches=""
+	print -P "$ZSH_THEME_SCM_PROMPT_PREFIX$ZSH_THEME_SCM_PROMPT_ORDER$ZSH_THEME_SCM_PROMPT_SUFFIX"
 }
 
 parse_git_dirty () {
-  gitstat=$(git status 2>/dev/null | grep '\(# Untracked\|# Changes\|# Changed but not updated:\)')
+  gitstat=$(echo $1 | grep '\(# Untracked\|# Changes\|# Changed but not updated:\)')
 
-  if [[ $(echo ${gitstat} | grep -c "^# Changes to be committed:$") > 0 ]]; then
+  if echo ${gitstat} | grep -q "^# Changes to be committed:$"; then
 	echo -n "$ZSH_THEME_SCM_PROMPT_DIRTY"
   fi
 
-  if [[ $(echo ${gitstat} | grep -c "^\(# Untracked files:\|# Changed but not updated:\)$") > 0 ]]; then
+  if echo ${gitstat} | grep -q "^\(# Untracked files:\|# Changed but not updated:\)$"; then
 	echo -n "$ZSH_THEME_SCM_PROMPT_UNTRACKED"
   fi 
 
   if [[ $(echo ${gitstat} | wc -l | tr -d ' ') == 0 ]]; then
 	echo -n "$ZSH_THEME_SCM_PROMPT_CLEAN"
+  fi
+}
+
+parse_git_branch_status () {
+  gitstat=$(echo $1 | grep "Your branch is")
+
+  if ${gitstat}; then
+	if echo ${gitstat} | grep -q "ahead"; then
+		echo -n "$ZSH_THEME_SCM_PROMPT_AHEAD"
+	elif echo ${gitstat} | grep "diverged"; then
+		echo -n "$ZSH_THEME_SCM_PROMPT_DIVERGED"
+	else
+		echo -n "$ZSH_THEME_SCM_PROMPT_BEHIND"
+	fi
   fi
 }
 
--- themes/arnold.zsh-theme
+++ themes/arnold.zsh-theme
@@ -1,4 +1,6 @@
-PROMPT='%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%}$(scm_prompt_info) # '
+PROMPT='%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%}$(scm_prompt_info) %# '
+
+ZSH_THEME_SCM_PROMPT_ORDER='${zsh_scm_info_branch}${zsh_scm_info_tags}${zsh_scm_info_dirty}${zsh_scm_info_branch_status}${zsh_scm_info_patches}'
 
 ZSH_THEME_SCM_PROMPT_PREFIX=""
 ZSH_THEME_SCM_PROMPT_SUFFIX="%{$reset_color%}"
@@ -7,7 +9,7 @@ ZSH_THEME_SCM_BRANCH_SUFFIX="%{$reset_color%}"
 ZSH_THEME_SCM_TAGS_PREFIX=" at %{$fg[yellow]%}"
 ZSH_THEME_SCM_TAGS_SEP="%{$reset_color%}, %{$fg[yellow]%}"
 ZSH_THEME_SCM_TAGS_SUFFIX="%{$reset_color%}"
-ZSH_THEME_SCM_PATCHES_PREFIX=" at %{$fg[yellow]%}"
+ZSH_THEME_SCM_PATCHES_PREFIX=" %{$fg[yellow]%}"
 ZSH_THEME_SCM_PATCHES_SEP="→"
 ZSH_THEME_SCM_PATCHES_PREAPPLIED="%{$fg[yellow]%}"
 ZSH_THEME_SCM_PATCHES_PREUNAPPLIED="%{$fg_bold[black]%}"
@@ -17,3 +19,6 @@ ZSH_THEME_SCM_PATCHES_SUFFIX="%{$reset_color%}"
 ZSH_THEME_SCM_PROMPT_DIRTY="%{$fg[green]%}!"
 ZSH_THEME_SCM_PROMPT_UNTRACKED="%{$fg[green]%}?"
 ZSH_THEME_SCM_PROMPT_CLEAN=""
+ZSH_THEME_SCM_PROMPT_AHEAD="↑"
+ZSH_THEME_SCM_PROMPT_DIVERGED="↕"
+ZSH_THEME_SCM_PROMPT_BEHIND="↓"
