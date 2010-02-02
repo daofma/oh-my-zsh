4ca9578306c790d12dbd913a02bf3e7627853ab4
David Arnold <daofma@gmail.com>
2010-02-01 20:50:51 -0500

    merged git_prompt_info and hg_prompt_info into usable scm_prompt_info altered env var names to reference "SCM" instead of git. created theme based on Steve Losh's to use new vars.

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,14 +1,14 @@
 # get the name of the branch we are on
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  echo "$ZSH_THEME_SCM_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_SCM_PROMPT_SUFFIX"
 }
 
 parse_git_dirty () {
   if [[ $((git status 2> /dev/null) | tail -n1) != "nothing to commit (working directory clean)" ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+    echo "$ZSH_THEME_SCM_PROMPT_DIRTY"
   else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+    echo "$ZSH_THEME_SCM_PROMPT_CLEAN"
   fi
 }
 
