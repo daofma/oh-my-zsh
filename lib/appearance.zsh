39c6becd91b6b7f9c41778e8d9943133de264411
David Arnold <daofma@gmail.com>
2010-02-03 19:34:33 -0500

    set some sane defaults for SCM prompt

--- lib/appearance.zsh
+++ lib/appearance.zsh
@@ -25,11 +25,39 @@ fi
 # Apply theming defaults
 PS1="%n@%m:%~%# "
 
-# git theming default: Variables for theming the git info prompt
-ZSH_THEME_GIT_PROMPT_PREFIX="git:("         # Prefix at the very beginning of the prompt, before the branch name
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"             # At the very end of the prompt
-ZSH_THEME_GIT_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
-ZSH_THEME_GIT_PROMPT_CLEAN=""               # Text to display if the branch is clean
+# version control theming default: Variables for theming the version control info prompt
+ZSH_THEME_SCM_PROMPT_PREFIX=":("            # Prefix at the very beginning of the prompt
+ZSH_THEME_SCM_PROMPT_SUFFIX=")"             # At the very end of the prompt
+ZSH_THEME_SCM_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
+ZSH_THEME_SCM_PROMPT_UNTRACKED=""           # Text to display if the branch has files which are not tracked
+ZSH_THEME_SCM_PROMPT_CLEAN=""               # Text to display if the branch is clean
+ZSH_THEME_SCM_PROMPT_SUFFIX=")"             # At the very end of the prompt
+ZSH_THEME_SCM_BRANCH_PREFIX=""              # At the beginning of the current branch
+ZSH_THEME_SCM_BRANCH_SUFFIX=""              # At the end of the current branch
+ZSH_THEME_SCM_TAGS_PREFIX=""                # Before any tags
+ZSH_THEME_SCM_TAGS_SEP=""                   # In between tags
+ZSH_THEME_SCM_TAGS_SUFFIX=""                # After all tags
+ZSH_THEME_SCM_PATCHES_PREFIX=""             # Before patches
+ZSH_THEME_SCM_PATCHES_SEP=""                # Between all patches
+ZSH_THEME_SCM_PATCHES_PREAPPLIED=""         # Before applied patches in queue
+ZSH_THEME_SCM_PATCHES_PREUNAPPLIED=""       # Before unapplied patches in queue
+ZSH_THEME_SCM_PATCHES_POSTAPPLIED=""        # After applied patches
+ZSH_THEME_SCM_PATCHES_POSTUNAPPLIED=""      # After unapplied patches
+ZSH_THEME_SCM_PATCHES_SUFFIX=""             # After all patches
+ZSH_THEME_SCM_PROMPT_AHEAD=""               # If the local branch is ahead of the remote branch
+ZSH_THEME_SCM_PROMPT_DIVERGED=""            # If the local branch is diverged from the remote branch
+ZSH_THEME_SCM_PROMPT_BEHIND=""              # If the local branch is behind the remote branch
+ZSH_THEME_SCM_PROMPT_GIT="git"              # If the current directory is in git
+ZSH_THEME_SCM_PROMPT_HG="hg"                # If the current directory is in mercurial
+
+# use the following variables to construct the SCM part of the prompt
+# ${zsh_scm_info_type}            the scm type (from git or hg above)
+# ${zsh_scm_info_branch}          the current branch (including prefix/suffix)
+# ${zsh_scm_info_tags}            current tags
+# ${zsh_scm_info_dirty}           dirty/clean/untracked status text
+# ${zsh_scm_info_branch_status}   ahead/diverged/behind status text
+# ${zsh_scm_info_patches}         list of patches in queue
+ZSH_THEME_SCM_PROMPT_ORDER='${zsh_scm_info_type}${zsh_scm_info_branch}${zsh_scm_info_tags}${zsh_scm_info_dirty}${zsh_scm_info_branch_status}${zsh_scm_info_patches}'
 
 # Setup the prompt with pretty colors
 setopt prompt_subst
