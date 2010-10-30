0b8ef30db3e1135981be44611073c03c524612cd
David Arnold <daofma@gmail.com>
2010-10-30 17:20:21 -0400

    fixed more hook registration errors

--- lib/plugins.zsh
+++ lib/plugins.zsh
@@ -1,56 +1,57 @@
 #!/usr/bin/env zsh
+#
 # Load all of the plugins that were defined in ~/.zshrc
 plugin=${plugin:=()}
 for plugin ($plugins);
 do
+    # Add the plugin's dir to fpath
 	fpath=($ZSH/plugins/$plugin $fpath)
-	source $ZSH/plugins/$plugin/$plugin.plugin.zsh
-done
 
-# Load hook functions from all of the plugins that were defined in ~/.zshrc
-plugin=${plugin:=()}
-for plugin ($plugins);
-do
+    # Source the plugin itself
+	source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+
+    # Load hook functions from all of the plugins that were defined in ~/.zshrc
+    
     # chpwd
     if [[ -a $ZSH/plugins/$plugin/${plugin}_chpwd ]];
     then
         autoload ${plugin}_chpwd
-        chpwd_functions=($precmd_functions ${plugin}_chpwd)
+        chpwd_functions=($chpwd_functions ${plugin}_chpwd)
     fi
 
-    #periodic
+    # periodic
     if [[ -a $ZSH/plugins/$plugin/${plugin}_periodic ]];
     then
         autoload ${plugin}_periodic
-        periodic_functions=($precmd_functions ${plugin}_periodic)
+        periodic_functions=($periodic_functions ${plugin}_periodic)
     fi
 
-    #precmd
+    # precmd
     if [[ -a $ZSH/plugins/$plugin/${plugin}_precmd ]];
     then
         autoload ${plugin}_precmd
         precmd_functions=($precmd_functions ${plugin}_precmd)
     fi
 
-    #preexec
+    # preexec
     if [[ -a $ZSH/plugins/$plugin/${plugin}_preexec ]];
     then
         autoload ${plugin}_preexec
-        preexec_functions=($precmd_functions ${plugin}_preexec)
+        preexec_functions=($preexec_functions ${plugin}_preexec)
     fi
 
-    #zshaddhistory
+    # zshaddhistory
     if [[ -a $ZSH/plugins/$plugin/${plugin}_zshaddhistory ]];
     then
         autoload ${plugin}_zshaddhistory
-        zshaddhistory_functions=($precmd_functions ${plugin}_zshaddhistory)
+        zshaddhistory_functions=($zshaddhistory_functions ${plugin}_zshaddhistory)
     fi
 
-    #zshexit
+    # zshexit
     if [[ -a $ZSH/plugins/$plugin/${plugin}_zshexit ]];
     then
         autoload ${plugin}_zshexit
-        zshexit_functions=($precmd_functions ${plugin}_zshexit)
+        zshexit_functions=($zshexit_functions ${plugin}_zshexit)
     fi
 
     # Hook any prompt_info functions into precmd as well
